58457898a1f5b2e6f6de04b6965a15abd667af86
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-20 16:25:21 +0700

    Add contextor

---
+++ app/src/main/java/com/monkey_monkey/monkeyvideoviewerandroid/MainApplication.java
@@ -0,0 +1,8 @@
+package com.monkey_monkey.monkeyvideoviewerandroid;
+
+/**
+ * Created by admin on 20/12/2017 AD.
+ */
+
+public class MainActivity {
+}
---
+++ app/src/main/java/com/monkey_monkey/monkeyvideoviewerandroid/util/Contextor.java
@@ -0,0 +1,8 @@
+package com.monkey_monkey.monkeyvideoviewerandroid.util;
+
+/**
+ * Created by admin on 20/12/2017 AD.
+ */
+
+public class Contextor {
+}
