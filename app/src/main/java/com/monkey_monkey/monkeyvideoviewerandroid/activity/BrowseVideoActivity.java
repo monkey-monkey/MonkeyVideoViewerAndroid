f7fe78ea75e2e69c4ebc9f08fd081b5bc4eb164d
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-21 14:48:22 +0700

    Add BrowseVideoActivity

---
+++ app/src/main/java/com/monkey_monkey/monkeyvideoviewerandroid/activity/BrowseVideoActivity.java
@@ -0,0 +1,13 @@
+package com.monkey_monkey.monkeyvideoviewerandroid.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BrowseVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_video);
+    }
+}
--- app/src/main/java/com/monkey_monkey/monkeyvideoviewerandroid/activity/MainActivity.java
+++ app/src/main/java/com/monkey_monkey/monkeyvideoviewerandroid/activity/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
@@ -29,6 +28,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         ImageButton keyboardBtn = findViewById(R.id.keyboard_btn);
         keyboardBtn.setOnClickListener(this);
+
+        ImageButton clockBtn = findViewById(R.id.clock_btn);
+        clockBtn.setOnClickListener(this);
+
+        ImageButton sheetBtn = findViewById(R.id.sheet_btn);
+        sheetBtn.setOnClickListener(this);
     }
 
 
@@ -42,6 +47,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent intent = new Intent(MainActivity.this, KeyboardActivity.class);
                 startActivityForResult(intent, REQUEST_KEYBOARD_INPUT);
                 break;
+            case R.id.clock_btn:
+                break;
+            case R.id.sheet_btn:
+                break;
             default:
                 break;
         }
@@ -52,19 +61,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
             case REQUEST_KEYBOARD_INPUT:
+                if (data.getStringExtra("studentID") != null) {
+                    startBrowseVideoActivity(data.getStringExtra("studentID"));
+                }
                 break;
             default:
                 IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
                 if (result != null) {
-                    if (result.getContents() == null) {
-                        Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                    if (result.getContents() != null) {
+                        startBrowseVideoActivity(result.getContents());
                     }
+//                    if (result.getContents() == null) {
+//                        Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+//                    } else {
+//                        Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+//                    }
                 } else {
                     super.onActivityResult(requestCode, resultCode, data);
                 }
                 break;
         }
+    }
+
+    private void startBrowseVideoActivity(String studentID) {
+
     }
 }
