82d11f9d7a887f255a5bf7f737ea124552868a35
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-26 16:51:59 +0700

    Change path to monkeyWeb

---
+++ app/src/main/java/com/monkey_monkey/monkeyvideoviewerandroid/dao/KeyStudentPathResponseDao.java
@@ -0,0 +1,8 @@
+package com.monkey_monkey.monkeyvideoviewerandroid.dao;
+
+/**
+ * Created by admin on 26/12/2017 AD.
+ */
+
+public class KeyStudentPathResponseDao {
+}
---
+++ app/src/main/java/com/monkey_monkey/monkeyvideoviewerandroid/manager/KeyStudentPathManager.java
@@ -0,0 +1,8 @@
+package com.monkey_monkey.monkeyvideoviewerandroid.manager;
+
+/**
+ * Created by admin on 26/12/2017 AD.
+ */
+
+public class KeyStudentPathManager {
+}
